f0573c32242172c1934f9c1f84faba9d0c6a631e
Hassanamiri <hassanamiri369@gmail.com>
2022-03-20 00:26:27 +0330

    added edit and delete and create function

--- src/context/context.js
+++ src/context/context.js
@@ -1,17 +1,39 @@
 import React , {createContext , useReducer} from "react"
 
+import nextId from "react-id-generator";
+
 export const ArticleContext = createContext()
 
 const initState = {
-    article : []
+    article : [
+        {id : nextId() , category : "Frontend" , heading : "WebDeveloper" , subHeading : "React developer", description : "this is frontend language" , author : "junior"}
+    ] ,
+
+   
 }
 
 const reducer = (state , action) =>{
     switch(action.type){
 
         // create 
+        case "createArticle":
+            return {...state , article : [...state.article , action.payload]}
         // delete
+        case "deleteArticle":
+            return {...state , article : state.article.filter(item => item.id !== action.payload.id)}
         // edit
+        case "editArticle" : 
+        const newEditArticle = action.payload;
+        const upDataArticle = state.article.map(item => {
+
+            if(item.id === newEditArticle.id){
+                return newEditArticle
+            }
+                return item;
+        })
+
+            return {...state , article : upDataArticle}
+
         default :
             return state;
     }
@@ -21,13 +43,16 @@ const ArticleProvider = (props) => {
 
     const [state , dispatch] = useReducer(reducer , initState)
 
+
+    // action.payload == article === {id : nextId() , category : "Frontend" , heading : "WebDeveloper" , subHeading : "React developer", description : "this is frontend language" , author : "junior"}
+
     const createArticle = article =>    dispatch({type : "createArticle" , payload : article})  
 
     const deleteArticle = article =>    dispatch({type : "deleteArticle" , payload : article})
 
     const editArticle = article  =>     dispatch({type : "editArticle" , payload : article})
 
-    const value = { state : state.article , createArticle , deleteArticle , editArticle}
+    const value = { article : state.article , createArticle , deleteArticle , editArticle}
 
     return(
         <ArticleContext.Provider value={value}>
