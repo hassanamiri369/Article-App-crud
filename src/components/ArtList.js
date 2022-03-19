bb1123a6064c7c4d95a045b8b0a2b51ea4371091
Hassanamiri <hassanamiri369@gmail.com>
2022-03-20 00:28:28 +0330

    show article component

--- src/components/ArtList.js
+++ src/components/ArtList.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React , {useContext} from 'react'
 import { Link } from 'react-router-dom'
+import { ArticleContext } from "../context/context"
 
 
-const ArtList = () => {
+const ArtList = (route) => {
+
+    const {article , deleteArticle , editArticle} = useContext(ArticleContext)
+
+    
+
 
   return (
     <>
         <div>ArtList</div>
         <Link to="/create" ><button>create article</button></Link>
+        <div className='container'>
+         
+            {article.map(item => (
+                    <div className='articleList' key={item.id}>
+                        <p>category : {item.category}</p>
+                        <p>heading : {item.heading}</p>
+                    
+                        <div>
+                            <span><button onClick={() => deleteArticle(item)}>delete</button></span>
+                            <span><Link to={`/detail/${item.id}`}><button>more info</button></Link></span>
+                            <span><Link to={`/edit/${item.id}`}><button onClick={() => editArticle(item)}>edit</button ></Link></span>
+                        </div>
+                    </div>
+                ))}
+            
+            
+               
+        </div>
     </>
   )
 }
