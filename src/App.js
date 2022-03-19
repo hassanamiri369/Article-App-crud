5776e822c880811fb3b0b6f0b85be117c98549c0
Hassanamiri <hassanamiri369@gmail.com>
2022-03-20 00:28:46 +0330

    added detail and edit route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import {BrowserRouter , Route , Switch } from "react-router-dom"
 import ArtList from "./components/ArtList";
 import ArtCreate from "./components/ArtCreate";
-
+import EditArticle from "./components/EditArticle";
+import Details from "./components/Details";
 function App() {
   return (
     <>
@@ -14,6 +15,8 @@ function App() {
       <Switch>
         <Route exact path="/"><ArtList/></Route>
         <Route path="/create"><ArtCreate/></Route>
+        <Route path="/detail/:id" ><Details/></Route>
+        <Route path="/edit/:user"><EditArticle/></Route>
       </Switch>
     </BrowserRouter>
     </>
