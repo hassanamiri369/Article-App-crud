2404f889bb6d89f1ea3e32cab0330cde4ae64961
Hassanamiri <hassanamiri369@gmail.com>
2022-03-19 11:50:42 +0330

    added simple route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import {BrowserRouter , Route , Switch } from "react-router-dom"
+import ArtList from "./components/ArtList";
+import ArtCreate from "./components/ArtCreate";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <div className="App">
       Hello
     </div>
+
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/"><ArtList/></Route>
+        <Route path="/create"><ArtCreate/></Route>
+      </Switch>
+    </BrowserRouter>
+    </>
   );
 }
 
