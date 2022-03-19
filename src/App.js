845b94823601cd554eac6741596fd4c263a7b90a
Hassanamiri <hassanamiri369@gmail.com>
2022-03-19 10:38:04 +0330

    first  commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import {BrowserRouter , Route , Switch } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
